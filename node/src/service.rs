22b39af7718837f4a2c75e920765f288186f7495
Steve Degosserie <steve@parity.io>
2021-11-11 23:31:27 +0100

    Updates from the node template (up to rev. #1ea0883)

--- node/src/service.rs
+++ node/src/service.rs
@@ -16,8 +16,12 @@ use trappist_runtime::{self, opaque::Block, RuntimeApi};
 pub struct ExecutorDispatch;
 
 impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
+	/// Only enable the benchmarking host functions when we actually want to benchmark.
+	#[cfg(feature = "runtime-benchmarks")]
 	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
-
+	/// Otherwise we only use the default Substrate host functions.
+	#[cfg(not(feature = "runtime-benchmarks"))]
+	type ExtendHostFunctions = ();
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
 		trappist_runtime::api::dispatch(method, data)
 	}
