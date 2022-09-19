35df979839baf59141f352c067c19c27797b7772
Bernardo A. Rodrigues <bernardo@parity.io>
2022-09-19 19:23:17 -0300

    rustfmt

--- runtime/src/xcm_config.rs
+++ runtime/src/xcm_config.rs
@@ -221,28 +221,29 @@ parameter_types! {
 
 //- From PR https://github.com/paritytech/cumulus/pull/936
 fn matches_prefix(prefix: &MultiLocation, loc: &MultiLocation) -> bool {
-	prefix.parent_count() == loc.parent_count() &&
-		loc.len() >= prefix.len() &&
-		prefix
+	prefix.parent_count() == loc.parent_count()
+		&& loc.len() >= prefix.len()
+		&& prefix
 			.interior()
 			.iter()
 			.zip(loc.interior().iter())
 			.all(|(prefix_junction, junction)| prefix_junction == junction)
 }
-
 pub struct ReserveAssetsFrom<T>(PhantomData<T>);
 impl<T: Get<MultiLocation>> FilterAssetLocation for ReserveAssetsFrom<T> {
 	fn filter_asset_location(asset: &MultiAsset, origin: &MultiLocation) -> bool {
 		let prefix = T::get();
 		log::trace!(target: "xcm::AssetsFrom", "prefix: {:?}, origin: {:?}", prefix, origin);
-		&prefix == origin &&
-			match asset {
-				MultiAsset { id: xcm::latest::AssetId::Concrete(asset_loc), fun: Fungible(_a) } =>
-					matches_prefix(&prefix, asset_loc),
+		&prefix == origin
+			&& match asset {
+				MultiAsset { id: xcm::latest::AssetId::Concrete(asset_loc), fun: Fungible(_a) } => {
+					matches_prefix(&prefix, asset_loc)
+				},
 				_ => false,
 			}
 	}
 }
+
 //--
 
 pub type Reserves = (NativeAsset, ReserveAssetsFrom<StatemineLocation>);
