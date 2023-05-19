bfd74ac9605559702d2b0b1f7f55716477600ea3
Bruno Galvao <brunopgalvao@gmail.com>
2023-05-19 14:06:31 -0400

    add tests for activating lockdown mode in GenesisConfig

--- pallets/lockdown-mode/src/tests.rs
+++ pallets/lockdown-mode/src/tests.rs
@@ -1,12 +1,28 @@
-use super::*;
+use super::{*, GenesisConfig};
 use crate::{mock::*, Error, ACTIVATED, DEACTIVATED};
 use frame_support::{assert_noop, assert_ok, traits::Contains};
 use pallet_balances::{self, Call as BalancesCall};
 use pallet_remark::{self, Call as RemarkCall};
 
 #[test]
+fn genesis_config_default() {
+	let default_genesis = GenesisConfig::default();
+	assert_eq!(default_genesis.activated, ACTIVATED);
+}
+
+#[test]
+fn genesis_config_initialized() {
+	[true, false].into_iter().for_each(|expected| {
+		new_test_ext(expected).execute_with(|| {
+			let lockdown_mode = LockdownModeStatus::<Test>::get();
+			assert_eq!(lockdown_mode, expected);
+		});
+	});
+}
+
+#[test]
 fn activate_lockdown_mode_works() {
-	new_test_ext().execute_with(|| {
+	new_test_ext(false).execute_with(|| {
 		assert_eq!(LockdownModeStatus::<Test>::get(), DEACTIVATED);
 		assert_ok!(LockdownMode::activate_lockdown_mode(RuntimeOrigin::root()));
 
@@ -22,7 +38,7 @@ fn activate_lockdown_mode_works() {
 
 #[test]
 fn deactivate_lockdown_mode_works() {
-	new_test_ext().execute_with(|| {
+	new_test_ext(false).execute_with(|| {
 		// We activate lockdown mode first so we can deactivate it.
 		assert_ok!(LockdownMode::activate_lockdown_mode(RuntimeOrigin::root()));
 
@@ -40,7 +56,7 @@ fn deactivate_lockdown_mode_works() {
 
 #[test]
 fn call_not_filtered_in_lockdown_mode() {
-	new_test_ext().execute_with(|| {
+	new_test_ext(false).execute_with(|| {
 		assert_ok!(LockdownMode::activate_lockdown_mode(RuntimeOrigin::root()));
 		let remark_call = RuntimeCall::Remark(RemarkCall::store { remark: vec![1, 2, 3] });
 		let result: bool = LockdownMode::contains(&remark_call);
@@ -50,7 +66,7 @@ fn call_not_filtered_in_lockdown_mode() {
 
 #[test]
 fn call_filtered_in_lockdown_mode() {
-	new_test_ext().execute_with(|| {
+	new_test_ext(false).execute_with(|| {
 		assert_ok!(LockdownMode::activate_lockdown_mode(RuntimeOrigin::root()));
 		let balance_call = RuntimeCall::Balance(BalancesCall::transfer { dest: 1, value: 2 });
 
@@ -61,7 +77,7 @@ fn call_filtered_in_lockdown_mode() {
 
 #[test]
 fn call_not_filtered_in_normal_mode() {
-	new_test_ext().execute_with(|| {
+	new_test_ext(false).execute_with(|| {
 		let lockdown_mode = LockdownModeStatus::<Test>::get();
 		assert_eq!(lockdown_mode, DEACTIVATED);
 		let balance_call = RuntimeCall::Balance(BalancesCall::transfer { dest: 1, value: 2 });
