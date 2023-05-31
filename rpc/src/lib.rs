3387f9f985e2bc3279d7aa3efd8bef14b8241969
Hector Bulgarini <hbulgarini@gmail.com>
2023-05-31 11:38:35 +0200

    Adding dex pallet to stout (#205)

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -86,6 +86,7 @@ where
 	Ok(module)
 }
 
+/// This function will be removed during the node refactor.
 /// Instantiate all RPCs we want at the canvas-kusama chain.
 pub fn stout_create_full<C, P>(
 	deps: FullDeps<C, P>,
@@ -101,9 +102,16 @@ where
 		+ 'static,
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
+	C::Api: pallet_dex_rpc::DexRuntimeApi<
+		trappist_runtime::opaque::Block,
+		trappist_runtime::AssetId,
+		trappist_runtime::Balance,
+		trappist_runtime::AssetBalance,
+	>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + Sync + Send + 'static,
 {
+	use pallet_dex_rpc::{Dex, DexApiServer};
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
 	use sc_rpc::dev::{Dev, DevApiServer};
 	use substrate_frame_rpc_system::{System, SystemApiServer};
@@ -114,6 +122,7 @@ where
 	module.merge(System::new(client.clone(), pool.clone(), deny_unsafe).into_rpc())?;
 	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
 	module.merge(Dev::new(client.clone(), deny_unsafe).into_rpc())?;
+	module.merge(Dex::new(client).into_rpc())?;
 
 	// Extend this RPC with a custom API by using the following syntax.
 	// `YourRpcStruct` should have a reference to a client, which is needed
