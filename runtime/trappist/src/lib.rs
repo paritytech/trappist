6168897c8811e7ea2f637b2f041e7313cb05f8e2
Steve Degosserie <steve@parity.io>
2023-07-06 16:00:20 +0200

    Bump runtime spec_version to 9370 (#246)

--- runtime/trappist/src/lib.rs
+++ runtime/trappist/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("trappist-rococo"),
 	impl_name: create_runtime_str!("trappist-rococo"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 9370,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
