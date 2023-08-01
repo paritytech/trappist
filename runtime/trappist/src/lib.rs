990ec067c77a548ea5890d1fc271dc95778d9bb1
Hector Bulgarini <hbulgarini@gmail.com>
2023-08-01 10:55:11 +0200

    Bump spec_version to 9420 and transaction_version to 2 (#253)
    * Bump spec_version to 9420
    
    * Bumping transaction verison

--- runtime/trappist/src/lib.rs
+++ runtime/trappist/src/lib.rs
@@ -134,10 +134,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("trappist-rococo"),
 	impl_name: create_runtime_str!("trappist-rococo"),
 	authoring_version: 1,
-	spec_version: 9370,
+	spec_version: 9420,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 	state_version: 1,
 };
 
