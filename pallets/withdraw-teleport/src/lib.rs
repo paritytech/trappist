95ce83c37f22c3be60b41d93636bb46e9d5c3971
metricaez <emiliano@parity.io>
2023-09-27 11:32:49 -0300

    improve pallet error wording

--- pallets/withdraw-teleport/src/lib.rs
+++ pallets/withdraw-teleport/src/lib.rs
@@ -66,7 +66,7 @@ pub mod pallet {
 		/// An error ocured during send
 		SendError,
 		/// Failed to execute
-		FailedToExecute,
+		FailedToExecuteXcm,
 	}
 
 	#[pallet::event]
@@ -216,7 +216,7 @@ impl<T: Config> Pallet<T> {
 		let hash = message.using_encoded(sp_io::hashing::blake2_256);
 		let outcome =
 			T::XcmExecutor::execute_xcm_in_credit(origin_location, message, hash, weight, weight);
-		outcome.clone().ensure_complete().map_err(|_| Error::<T>::FailedToExecute)?;
+		outcome.clone().ensure_complete().map_err(|_| Error::<T>::FailedToExecuteXcm)?;
 		Self::deposit_event(Event::Attempted { outcome });
 
 		// Use pallet-xcm send for sending message.
