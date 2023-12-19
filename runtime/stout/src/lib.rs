31306bf1d4fb1f60c91abd906541723c268948e8
metricaez <emiliano@parity.io>
2023-12-19 13:18:13 -0300

    increase spec ver

--- runtime/stout/src/lib.rs
+++ runtime/stout/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("stout-rococo"),
 	impl_name: create_runtime_str!("stout-rococo"),
 	authoring_version: 1,
-	spec_version: 11000,
+	spec_version: 13000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
--- runtime/trappist/src/lib.rs
+++ runtime/trappist/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("trappist-rococo"),
 	impl_name: create_runtime_str!("trappist-rococo"),
 	authoring_version: 1,
-	spec_version: 11000,
+	spec_version: 13000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
