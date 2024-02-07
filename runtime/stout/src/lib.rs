7ae093eb689f9bbd01c500230c929e9485165428
metricaez <emiliano@parity.io>
2024-02-07 09:23:09 -0300

    choir: bump spec ver

--- runtime/stout/src/lib.rs
+++ runtime/stout/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("stout-rococo"),
 	impl_name: create_runtime_str!("stout-rococo"),
 	authoring_version: 1,
-	spec_version: 13000,
+	spec_version: 13100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
